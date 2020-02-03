9a218da0bfc6adb5419a0a13eea148c006de8289
Dauren_Bablanov <bablanov.dauren@yandex.kz>
2020-02-03 22:56:22 +0600

    Проверка на равенство обектов

--- app/src/Dollar.php
+++ app/src/Dollar.php
@@ -2,17 +2,41 @@
 
 namespace src;
 
+/**
+ * Class Dollar
+ * @package src
+ */
 class Dollar
 {
+    /**
+     * @var int
+     */
     public int $amount = 0;
 
+    /**
+     * Dollar constructor.
+     * @param int $amount
+     */
     public function __construct(int $amount)
     {
         $this->amount = $amount;
     }
 
-    public function times(int $multiplier): self
+    /**
+     * @param int $multiplier
+     * @return $this
+     */
+    public function times(int $multiplier) : self
     {
         return new self($this->amount * $multiplier);
+    }
+
+    /**
+     * @param Dollar $dollar
+     * @return bool
+     */
+    public function equals(self $dollar) : bool
+    {
+        return $this->amount === $dollar->amount;
     }
 }
