c69b9a5489210690adf7e7f727b85f7cf12fa7f6
Dauren_Bablanov <bablanov.dauren@yandex.kz>
2020-02-04 22:33:03 +0600

    Проверка классов сравниваемых объектов

--- app/src/Money.php
+++ app/src/Money.php
@@ -15,6 +15,6 @@ class Money
      */
     public function equals(self $money) : bool
     {
-        return $this->amount === $money->amount;
+        return $this->amount === $money->amount &&  get_class($this) === get_class($money);
     }
 }
