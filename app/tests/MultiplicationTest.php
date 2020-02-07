537b0d87e484c05f4f225cf769c2b9ea5ee41bc1
Dauren Bablanov <bablanov.d@rahmetapp.kz>
2020-02-07 12:09:08 +0600

    Удалены классы Dollar и Franc

--- app/tests/MultiplicationTest.php
+++ app/tests/MultiplicationTest.php
@@ -12,8 +12,6 @@ class MultiplicationTest extends TestCase
     {
         $this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
         $this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
-        $this->assertTrue((Money::franc(5))->equals(Money::franc(5)));
-        $this->assertFalse((Money::franc(5))->equals(Money::franc(6)));
         $this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
     }
 }
